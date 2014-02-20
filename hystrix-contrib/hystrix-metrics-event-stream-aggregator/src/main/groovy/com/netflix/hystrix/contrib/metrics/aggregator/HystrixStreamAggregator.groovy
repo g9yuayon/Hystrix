0601430cb60621013d2cb4753dd831025554b49f
Ben Christensen <benjchristensen@netflix.com>
2014-02-20 10:36:02 -0800

    Observable<Observable<T>> for each instance

--- hystrix-contrib/hystrix-metrics-event-stream-aggregator/src/main/groovy/com/netflix/hystrix/contrib/metrics/aggregator/HystrixStreamAggregator.groovy
+++ hystrix-contrib/hystrix-metrics-event-stream-aggregator/src/main/groovy/com/netflix/hystrix/contrib/metrics/aggregator/HystrixStreamAggregator.groovy
@@ -11,11 +11,16 @@ class HystrixStreamAggregator {
     public static void main(String[] args) {
     }
 
-    public void run(Observable<Map<String, Object>> stream) {
+    /**
+     * 
+     * @param stream The inner Observable<Map<String, Object>> represents a single instance (network connection)
+     * @return
+     */
+    public Observable<Map<String, Object>> aggregate(Observable<Observable<Map<String, Object>>> stream) {
 
         System.out.println("Run HystrixStreaming Job!");
 
-        Observable<Map<String, Object>> output = stream.filter({ Map<String, Object> data ->
+        return stream.filter({ Map<String, Object> data ->
             return data.get("type").equals("HystrixCommand");
         }).groupBy({ Map<String, Object> json ->
             return String.valueOf(json.get("name"));
@@ -41,6 +46,7 @@ class HystrixStreamAggregator {
                         Number d = (Number) delta.get(key);
                         state.put(key, v.longValue() + d.longValue());
                     } else {
+                        // TODO handle strings
                         state.put(key, delta.get(key));
                     }
                 }
@@ -48,60 +54,47 @@ class HystrixStreamAggregator {
             }).throttleLast(500, TimeUnit.MILLISECONDS);
         });
 
-        output.filter({ Map<String, Object> data ->
-            return data.get("rollingCountFailure") != 0;
-//            return data.get("name") != null && data.get("name").equals("Search");
-        }).toBlockingObservable().forEach({ Map<String, Object> t1 ->
-            if (t1.keySet().size() > 0) {
-                System.out.println("     Sum [" + t1.get("name") + "] =>    Success => " + t1.get("rollingCountSuccess") + "  Failure => " + t1.get("rollingCountFailure"));
-            }
-        });
     }
 
 
     private final Func1<List<Map<String, Object>>, Map<String, Object>> previousAndCurrentToDelta() {
-        return new Func1<List<Map<String, Object>>, Map<String, Object>>() {
-
-            private Map<String, Object> delta = null;
-
-            @Override
-            public Map<String, Object> call(List<Map<String, Object>> data) {
-                Map<String, Object> previous = data.get(0);
-                if (data.size() < 2) {
-                    System.out.println("NO DATA");
-                    return Collections.emptyMap();
-                }
-                Map<String, Object> current = data.get(1);
-
-                if (delta == null) {
-                    delta = new HashMap<String, Object>();
-                    delta.putAll(previous);
-                    delta.put("instanceId", current.get("instanceId"));
-                    delta.put("name", current.get("name"));
-                }
-
-                for (String key : current.keySet()) {
-                    if (key.equals("instanceId")) {
-                        continue;
-                    }
-                    Object previousValue = previous.get(key);
-                    Object currentValue = current.get(key);
-                    if (currentValue instanceof Number) {
-                        if (previousValue == null) {
-                            previousValue = 0;
-                        }
-                        Number previousValueAsNumber = (Number) previousValue;
-                        if (currentValue != null) {
-                            Number currentValueAsNumber = (Number) currentValue;
-                            delta.put(key, (currentValueAsNumber.longValue() - previousValueAsNumber.longValue()));
-                        }
-                    } else {
-                        // do string ... ignore for now
-                    }
-                }
-
-                return delta;
+        Map<String, Object> delta = null;
+        return { List<Map<String, Object>> data ->
+            Map<String, Object> previous = data.get(0);
+            if (data.size() < 2) {
+                System.out.println("NO DATA");
+                return Collections.emptyMap();
             }
+            Map<String, Object> current = data.get(1);
+
+            if (delta == null) {
+                delta = new HashMap<String, Object>();
+                delta.putAll(previous);
+                delta.put("instanceId", current.get("instanceId"));
+                delta.put("name", current.get("name"));
+            }
+
+            for (String key : current.keySet()) {
+                if (key.equals("instanceId")) {
+                    continue;
+                }
+                Object previousValue = previous.get(key);
+                Object currentValue = current.get(key);
+                if (currentValue instanceof Number) {
+                    if (previousValue == null) {
+                        previousValue = 0;
+                    }
+                    Number previousValueAsNumber = (Number) previousValue;
+                    if (currentValue != null) {
+                        Number currentValueAsNumber = (Number) currentValue;
+                        delta.put(key, (currentValueAsNumber.longValue() - previousValueAsNumber.longValue()));
+                    }
+                } else {
+                    // do string ... ignore for now
+                }
+            }
+
+            return delta;
         }
     }
 }
--- hystrix-contrib/hystrix-metrics-event-stream-aggregator/src/test/groovy/com/netflix/hystrix/contrib/metrics/aggregator/HystrixStreamAggregatorTest.groovy
+++ hystrix-contrib/hystrix-metrics-event-stream-aggregator/src/test/groovy/com/netflix/hystrix/contrib/metrics/aggregator/HystrixStreamAggregatorTest.groovy
@@ -12,15 +12,22 @@ class HystrixStreamAggregatorTest {
     public static void main(String[] args) {
         new HystrixStreamAggregatorTest().demo();
     }
-    
+
     @Test
     public void demo() {
-        
+
         Observable<Map<String, Object>> hystrixStreamA = HystrixStreamSource.getHystrixStream(12345).subscribeOn(Schedulers.newThread());
         Observable<Map<String, Object>> hystrixStreamB = HystrixStreamSource.getHystrixStream(67890).skip(400).subscribeOn(Schedulers.newThread());
 
-        Observable<Map<String, Object>> fullStream = Observable.merge(hystrixStreamA, hystrixStreamB);
-        
-        new HystrixStreamAggregator().run(fullStream);
+        Observable<Map<String, Object>> fullStream = Observable.parallelMerge(hystrixStreamA, hystrixStreamB);
+
+        new HystrixStreamAggregator().aggregate(fullStream).filter({ Map<String, Object> data ->
+            return data.get("rollingCountFailure") != 0;
+            //            return data.get("name") != null && data.get("name").equals("Search");
+        }).toBlockingObservable().forEach({ Map<String, Object> t1 ->
+            if (t1.keySet().size() > 0) {
+                System.out.println("     Sum [" + t1.get("name") + "] =>    Success => " + t1.get("rollingCountSuccess") + "  Failure => " + t1.get("rollingCountFailure"));
+            }
+        });
     }
 }
